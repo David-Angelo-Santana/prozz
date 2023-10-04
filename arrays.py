336b5ffd325cc2adb9204dc8fb25bed982c6a945
David Sanatana <111399288+David-Angelo-Santana@users.noreply.github.com>
2023-10-04 18:46:48 -0300

    arrays

---
+++ arrays.py
@@ -0,0 +1,9 @@
+# Array de produtos de uma loja de comida
+produtos_comida = ["Arroz", "Feijão", "Macarrão", "Carne", "Legumes"]
+
+# Array de anos de nascimento de familiares e amigos
+anos_nascimento = [1990, 1985, 2000, 1995, 1980]
+
+# Imprimindo os arrays no terminal
+print("Produtos da loja de comida:", produtos_comida)
+print("Anos de nascimento de familiares e amigos:", anos_nascimento)
